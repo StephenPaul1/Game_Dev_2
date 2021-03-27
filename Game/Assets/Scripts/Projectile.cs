5852f47fe91388151c6fa3e52b5b958254aed3dd
StephenPaul1 <79176526+StephenPaul1@users.noreply.github.com>
2021-03-27 18:08:48 +1100

    Game_Dev_2

---
+++ Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody rbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody.AddForce(rbody.transform.forward * speed, ForceMode.Impulse);
+        Destroy(gameObject, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        {
+            print(hit.collider.name);
+
+            if (hit.collider.CompareTag("Target"))
+            {
+                hit.collider.GetComponent<Renderer>().material.color = Color.blue;
+            }
+        }
+    }
+}
---
+++ Game/Assets/Scripts/Target.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if (collision.collider.CompareTag("Projectile"))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            Destroy(gameObject, 1f);
+            Destroy(collision.gameObject);
+
+
+        }
+
+
+        // collision.collider.GetComponent<Renderer>().material.color = Color.red;
+        //  Destroy(collision.collider.gameObject, 1f);
+        //  Destroy(this.gameObject);
+    }
+
+    
+        }
+
+
